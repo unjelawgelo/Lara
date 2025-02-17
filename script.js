5f7b46f675ea55da9f2662c1c78f74f10d9e5cb6
unjelawgelo <mariano.mikeangeloj@gmail.com>
2025-02-17 22:32:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,6 @@ document.addEventListener("DOMContentLoaded", () => {
             imageUpload.click();
         });
     }
-    d
     function switchTab(clickedTab) {
         document.querySelectorAll(".tab").forEach((tab) => tab.classList.remove("active"));
         document.querySelectorAll(".content").forEach((content) => content.classList.remove("active"));
